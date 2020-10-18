ed96ea2e04acbf89ff8d0140cab033b9eab588b4
Anastasiia <anastacia.bilyk@gmail.com>
2020-10-18 22:21:32 +0300

    Change Id to String.

--- src/main/java/com/practice/mongodb/repository/ForecastRepository.java
+++ src/main/java/com/practice/mongodb/repository/ForecastRepository.java
@@ -3,5 +3,5 @@ package com.practice.mongodb.repository;
 import com.practice.mongodb.document.Forecast;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ForecastRepository extends MongoRepository<Forecast, Long> {
+public interface ForecastRepository extends MongoRepository<Forecast, String> {
 }
