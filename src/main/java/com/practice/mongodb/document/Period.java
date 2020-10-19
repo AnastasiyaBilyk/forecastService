eb0e6a6781ef34f3b5a3866c33357d638ce8f798
Anastasiia <anastacia.bilyk@gmail.com>
2020-10-19 21:51:40 +0300

    Add lombok to document classes.

--- src/main/java/com/practice/mongodb/document/Period.java
+++ src/main/java/com/practice/mongodb/document/Period.java
@@ -1,12 +1,15 @@
 package com.practice.mongodb.document;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
+@Data
 public class Period {
     int number;
     String name;
     String startTime;
     String endTime;
+    @JsonProperty(value = "isDaytime")
     boolean isDaytime;
     int temperature;
     String temperatureUnit;
@@ -16,109 +19,4 @@ public class Period {
     String icon;
     String shortForecast;
     String detailedForecast;
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-
-    @JsonProperty(value = "isDaytime")
-    public boolean isDaytime() {
-        return isDaytime;
-    }
-
-    public void setDaytime(boolean daytime) {
-        isDaytime = daytime;
-    }
-
-    public int getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(int temperature) {
-        this.temperature = temperature;
-    }
-
-    public String getTemperatureUnit() {
-        return temperatureUnit;
-    }
-
-    public void setTemperatureUnit(String temperatureUnit) {
-        this.temperatureUnit = temperatureUnit;
-    }
-
-    public String getTemperatureTrend() {
-        return temperatureTrend;
-    }
-
-    public void setTemperatureTrend(String temperatureTrend) {
-        this.temperatureTrend = temperatureTrend;
-    }
-
-    public String getWindSpeed() {
-        return windSpeed;
-    }
-
-    public void setWindSpeed(String windSpeed) {
-        this.windSpeed = windSpeed;
-    }
-
-    public String getWindDirection() {
-        return windDirection;
-    }
-
-    public void setWindDirection(String windDirection) {
-        this.windDirection = windDirection;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-
-    public String getShortForecast() {
-        return shortForecast;
-    }
-
-    public void setShortForecast(String shortForecast) {
-        this.shortForecast = shortForecast;
-    }
-
-    public String getDetailedForecast() {
-        return detailedForecast;
-    }
-
-    public void setDetailedForecast(String detailedForecast) {
-        this.detailedForecast = detailedForecast;
-    }
 }
