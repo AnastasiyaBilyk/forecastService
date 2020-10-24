06a8f03ea99c87cd9f54464d3542d920f9ff5ae9
Anastasiia <anastacia.bilyk@gmail.com>
2020-10-24 18:05:41 +0300

    Move @ComponentScan to application class.

--- src/main/java/com/practice/mongodb/ApplicationConfiguration.java
+++ src/main/java/com/practice/mongodb/ApplicationConfiguration.java
@@ -3,7 +3,6 @@ package com.practice.mongodb;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +11,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
-@ComponentScan
 public class ApplicationConfiguration {
 
     @Bean
