066b18dfbceebebc9d02c0ae2b05ea5db6bf952c
Anastasiia <anastacia.bilyk@gmail.com>
2020-10-24 18:05:41 +0300

    Move @ComponentScan to application class.

--- src/main/java/com/practice/mongodb/MongodbApplication.java
+++ src/main/java/com/practice/mongodb/MongodbApplication.java
@@ -2,10 +2,11 @@ package com.practice.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.practice")
 @SpringBootApplication
 public class MongodbApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MongodbApplication.class, args);
 	}
