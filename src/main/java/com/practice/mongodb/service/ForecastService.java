251947b0b7acb1c7edf887e88a980896efb82113
Anastasiia <anastacia.bilyk@gmail.com>
2020-10-27 22:13:13 +0200

    Added ability to send and consume messages.

--- src/main/java/com/practice/mongodb/service/ForecastService.java
+++ src/main/java/com/practice/mongodb/service/ForecastService.java
@@ -2,10 +2,12 @@ package com.practice.mongodb.service;
 
 import com.practice.mongodb.document.Forecast;
 
+import java.util.Set;
 import java.util.UUID;
 
 public interface ForecastService {
 
-    void saveForecast(double lat, double lon, UUID uuid);
+    void saveForecast(double lat, double lon, UUID uuid, String clientId);
     Forecast getForecast(UUID uuid);
+    Set<Forecast> getForecasts(String clientId);
 }
