209a0d33a237d83409418f65a384e162bfce4e75
Anastasiia <anastacia.bilyk@gmail.com>
2020-10-27 22:13:13 +0200

    Added ability to send and consume messages.

--- src/main/java/com/practice/mongodb/service/ForecastServiceImpl.java
+++ src/main/java/com/practice/mongodb/service/ForecastServiceImpl.java
@@ -3,11 +3,13 @@ package com.practice.mongodb.service;
 import com.practice.mongodb.document.Forecast;
 import com.practice.mongodb.repository.ForecastRepository;
 import com.practice.mongodb.service.api.client.ApiWeatherClient;
+import com.practice.mongodb.service.messaging.MessagingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PreDestroy;
 import java.io.IOException;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 
@@ -15,23 +17,28 @@ import java.util.concurrent.ExecutorService;
 public class ForecastServiceImpl implements ForecastService {
 
     private final ForecastRepository forecastRepository;
-
     private final ExecutorService executorService;
-
     private final ApiWeatherClient apiWeatherClient;
+    private final MessagingService<String, Forecast> kafkaMessagingService;
 
     @Autowired
-    public ForecastServiceImpl(ForecastRepository forecastRepository, ExecutorService executorService, ApiWeatherClient apiWeatherClient) {
+    public ForecastServiceImpl(ForecastRepository forecastRepository,
+                               ExecutorService executorService,
+                               ApiWeatherClient apiWeatherClient,
+                               MessagingService<String, Forecast> kafkaMessagingService) {
         this.forecastRepository = forecastRepository;
         this.executorService = executorService;
         this.apiWeatherClient = apiWeatherClient;
+        this.kafkaMessagingService = kafkaMessagingService;
     }
 
     @Override
-    public void saveForecast(double lat, double lon, UUID uuid) {
+    public void saveForecast(double lat, double lon, UUID uuid, String clientId) {
         executorService.submit(() -> {
             try {
-                forecastRepository.save(apiWeatherClient.getForecast(uuid, lat, lon));
+                Forecast forecast = apiWeatherClient.getForecast(uuid, lat, lon);
+                forecastRepository.save(forecast);
+                kafkaMessagingService.sendToQueue(clientId, forecast);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -41,6 +48,11 @@ public class ForecastServiceImpl implements ForecastService {
     @Override
     public Forecast getForecast(UUID uuid) {
         return forecastRepository.findById(uuid).orElse(null);
+    }
+
+    @Override
+    public Set<Forecast> getForecasts(String clientId) {
+        return kafkaMessagingService.getMessages(clientId);
     }
 
     @PreDestroy
